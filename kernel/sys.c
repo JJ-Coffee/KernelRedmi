b7a341c0cfa081e8ac01c0e946f23f8bd63d1ed1
Milo <107900975+JJ-Coffee@users.noreply.github.com>
2024-12-29 19:12:55 +0700

    Add files via upload

--- kernel/sys.c
+++ kernel/sys.c
@@ -1186,21 +1186,18 @@ static int override_release(char __user *release, size_t len)
 }
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern int susfs_spoof_uname(struct new_utsname* tmp);
+extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
-
-
 static uint64_t netbpfload_pid = 0;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	if (likely(!susfs_spoof_uname(&tmp)))
-		goto bypass_orig_flow;
-#endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	if (!strncmp(current->comm, "netbpfload", 10) &&
 	    current->pid != netbpfload_pid) {
 		netbpfload_pid = current->pid;
@@ -1208,9 +1205,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-bypass_orig_flow:
-#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
